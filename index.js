a78b251d9bd6e20bf6278b6d0b5a5cc55d1fdffc
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2024-10-01 15:35:26 -0700

    responsive navbar

---
+++ index.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".responsive-nav-items")
+function showSidebar() {
+    sidebar.style.visibility = "visible"
+    // sidebar.style.transition = "visibility 0.5s ease-in-out"
+}
+function closeSidebar() {
+ sidebar.style.visibility = "hidden"
+//  sidebar.style.transition = "visibility 0.5s ease-in-out"
+}
